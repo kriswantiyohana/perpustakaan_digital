9160e188738176b8b1d78272fe519b5afaf9c1b4
Markus <Markus.sianturi@binus.ac.id>
2023-11-24 16:29:21 +0700

    submission 2

---
+++ resources/views/login.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan digital | Login </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<style>
+    .main {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+
+    .login-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 30px;
+    }
+</style>
+<body>
+    <div class="main d-flex justify-content-center align-items-center">
+        <div class="login-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="usernme" id="username" class="form-control">
+                </div>
+                <div>
+                    <label for="password" class="form-label"> Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+            </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
